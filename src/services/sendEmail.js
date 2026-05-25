e96426ad15cf9b84df836cfc7306d74b66b5c92a
Juan Esteban Ortiz <juanestebanortizbejarano@gmail.com>
2026-05-24 19:41:15 -0500

    email dri fixed

--- src/services/sendEmail.js
+++ src/services/sendEmail.js
@@ -1,7 +1,7 @@
 const nodemailer = require('nodemailer');
 require('dotenv').config();
 
-const CORREO_DRI="brs77ortiz@hotmail.com"
+const CORREO_DRI="samuel.ramirez@correounivalle.edu.co"
 
 let cachedTransporter;
 
